38d589fe1574c6662f97a3339762c4973598047d
Chickquak <160209414+Chickquak@users.noreply.github.com>
2025-04-09 14:26:12 +0000

    Pending changes exported from your codespace

---
+++ my-drupal-site/web/sites/default/files/php/trash/Drupal__node__NodeStorageTrash67f334bc4733a/C7tHNqgZVQDjnArPSDn0TC1pXnM_-Hjn_jx-yWCSKt0.php
@@ -0,0 +1,12 @@
+<?php
+
+use Drupal\trash\TrashStorageTrait;
+
+/**
+ * Provides a custom storage class for trash-enabled entity types.
+ */
+class Drupal__node__NodeStorageTrash67f334bc4733a extends \Drupal\node\NodeStorage {
+
+  use TrashStorageTrait;
+
+}
---
+++ my-drupal-site/web/sites/default/files/php/trash/Drupal__node__NodeStorageTrash67f5ff44b52c9/V3N0sjKOMPev-PiSxor5hw9kD3hqNJO0hEeDJ1xPLWs.php
@@ -0,0 +1,12 @@
+<?php
+
+use Drupal\trash\TrashStorageTrait;
+
+/**
+ * Provides a custom storage class for trash-enabled entity types.
+ */
+class Drupal__node__NodeStorageTrash67f5ff44b52c9 extends \Drupal\node\NodeStorage {
+
+  use TrashStorageTrait;
+
+}
